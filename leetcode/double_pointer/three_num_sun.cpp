3dc04b90a052f5c72537fa3fe50618585a8edf5d
tadylon <enthuswine@gmail.com>
2026-08-09 15:09:43 +0800

    add a demo

---
+++ leetcode/double_pointer/three_num_sun.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        for (int i : nums) {
+            for (int m : nums) {
+                for (int n : nums) {
+                    
+                }
+            }
+        }
+    }
+};
